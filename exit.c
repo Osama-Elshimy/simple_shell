6aaa5e8c79bf7d7cfbca901dc5f5c50c57ca51a4
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-12-04 03:58:02 +0200

    handle exit status

--- exit.c
+++ exit.c
@@ -8,6 +8,19 @@
 
 void exit_builtin(char **argv)
 {
+	int status = 0;
+
+	if (argv[1] != NULL)
+	{
+		status = atoi(argv[1]);
+
+		if (status == 0 && strcmp(argv[1], "0") != 0)
+		{
+			fprintf(stderr, "exit: %s: numeric argument required\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
 	string_array_free(&argv);
-	exit(0);
+	exit(status);
 }
